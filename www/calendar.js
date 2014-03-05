359ae7a3b33ea13dab1dbd366e01a173d0e23008
thapeloa <thapelo.afrika@telestream.co.za>
2014-03-05 10:56:42 +0200

    Calendar test
    Calendar test

---
+++ www/calendar.js
@@ -0,0 +1,55 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+*/
+
+/*
+var cordova = require('cordova'),
+exec = require('cordova/exec');
+
+cordova.exec(
+    function onSuccess() {
+     alert("Started java thread");
+    },
+    function(error) {
+    	alert("Failed to start thread:"+error);
+    },
+    "JavaThread",
+    "action",
+    []);
+    
+module.exports = javathread;    
+*/
+var calendarPlugin = {
+    createEvent: function(title, location, notes, startDate, endDate, successCallback, errorCallback) {
+        cordova.exec(
+            successCallback, // success callback function
+            errorCallback, // error callback function
+            'CalendarPlugin', // mapped to our native Java class called "CalendarPlugin"
+            'addCalendarEntry', // with this action name
+            [{                  // and this array of custom arguments to create our entry
+                "title": title,
+                "description": notes,
+                "eventLocation": location,
+                "startTimeMillis": startDate.getTime(),
+                "endTimeMillis": endDate.getTime()
+            }]
+        ); 
+     }
+}
